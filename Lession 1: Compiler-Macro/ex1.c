d5aa439fbb923546eeb7db33b5095498aee27021
mrhoang18 <151119831+mrhoang18@users.noreply.github.com>
2024-08-15 03:36:34 +0700

    Update ex1.c

--- Lession 1: Compiler-Macro/ex1.c
+++ Lession 1: Compiler-Macro/ex1.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#define tong(...) sum(__VA_ARGS__,0)
+
+int sum(int count, ...){
+    va_list va;
+
+    int sum = count;
+
+    va_start(va, count);
+
+    int value;
+
+    while ((value=va_arg(va, int))!=0)
+    {
+        sum+=value;
+    }
+    
+    va_end(va);
+
+    return sum;
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("tong %d\n", tong(1,2,3,4,5,6));
+    return 0;
+}
 
