f771c28f3880dd95415afeaba1d3776e1c83bf00
mrhoang18 <151119831+mrhoang18@users.noreply.github.com>
2024-08-19 14:32:13 +0700

    Update ex1.c

--- Lession 2: STDARG - ASSERT/ex1.c
+++ Lession 2: STDARG - ASSERT/ex1.c
@@ -1,1 +1,78 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef enum {
+    TEMPERATURE_SENSOR,
+    PRESSURE_SENSOR,
+    //add
+    FUEL_SENSOR,
+    SPEED_SENSOR
+} SensorType;
+
+void processSensorData(SensorType type, ...) {
+    va_list args;
+    va_start(args, type);
+
+    switch (type) {
+        case TEMPERATURE_SENSOR: {
+            int numArgs = va_arg(args, int);
+            int sensorId = va_arg(args, int);
+            float temperature = va_arg(args, double); // float được promote thành double
+            printf("Temperature Sensor ID: %d, Reading: %.2f degrees\n", sensorId, temperature);
+            if (numArgs > 2) {
+                // Xử lý thêm tham số nếu có
+                char* additionalInfo = va_arg(args, char*);
+                printf("Additional Info: %s\n", additionalInfo);
+            }
+            break;
+        }
+        case PRESSURE_SENSOR: {
+            int numArgs = va_arg(args, int);
+            int sensorId = va_arg(args, int);
+            int pressure = va_arg(args, int);
+            printf("Pressure Sensor ID: %d, Reading: %d Pa\n", sensorId, pressure);
+            if (numArgs > 2) {
+                // Xử lý thêm tham số nếu có
+                char* unit = va_arg(args, char*);
+                printf("Unit: %s\n", unit);
+            }
+            break;
+        }
+        case FUEL_SENSOR: {
+            int numArgs = va_arg(args, int);
+            int sensorId = va_arg(args, int);
+            float fuelLevel = va_arg(args, double);
+            printf("Fuel sensor ID: %d, Reading: %.2f litter\n", sensorId, fuelLevel);
+            if (numArgs > 2) {
+                // Xử lý thêm tham số nếu có
+                char* unit = va_arg(args, char*);
+                printf("Additional Info: %s\n", unit);
+            }
+            break;
+        }
+        case SPEED_SENSOR: {
+            int numArgs = va_arg(args, int);
+            int sensorId = va_arg(args, int);
+            float speed = va_arg(args, double);
+            printf("Speed Sensor ID: %d, Reading: %.2f km/h\n", sensorId, speed);
+            if (numArgs > 2) {
+                // Xử lý thêm tham số nếu có
+                char* unit = va_arg(args, char*);
+                printf("Unit: %s\n", unit);
+            }
+            break;                        
+        }
+    }
+
+    va_end(args);
+}
+int main() {
+    processSensorData(TEMPERATURE_SENSOR, 3, 1, 36.5, "Room Temperature");
+    processSensorData(PRESSURE_SENSOR, 2, 2, 101325);
+    processSensorData(FUEL_SENSOR, 3, 3, 50.0, "Fuel tank half full");
+    processSensorData(SPEED_SENSOR, 2, 4, 120.0);
+    return 0;
+}
+
+
 
